7423370d42ccb74f6d1611c9be57195f611705e9
SWATHY RAMAKRISHNAN <sujswa@gmail.com>
2026-01-07 14:25:34 +0100

    Add files via upload

---
+++ router.py
@@ -0,0 +1,32 @@
+from semantic_router import Route, RouteLayer
+from semantic_router.encoders import HuggingFaceEncoder
+
+encoder = HuggingFaceEncoder(
+    name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+faq = Route(
+    name='faq',
+    utterances=[
+        "What is the return policy of the products?",
+        "Do I get discount with the HDFC credit card?",
+        "How can I track my order?",
+        "What payment methods are accepted?",
+        "How long does it take to process a refund?",
+    ]
+)
+
+sql = Route(
+    name='sql',
+    utterances=[
+        
+        "Are there any books on sale?",
+        "What is the price ofThen She Was Gone: A Novel	?",
+    ]
+)
+
+router = RouteLayer(routes=[faq, sql], encoder=encoder)
+
+if __name__ == "__main__":
+    print(router("What is your policy on defective product?").name)
+    print(router("Pink Puma shoes in price range 5000 to 1000").name)
